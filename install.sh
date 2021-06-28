472b9a87785e30481a98195b42f2f5cf73771e5f
ivanol55 <ivanm106@gmail.com>
2021-06-28 13:20:08 +0200

    update del install file

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 su root -c "apt update && apt install i3 volumeicon-alsa feh xset imagemagick compton"
-cp -r $(pwd)/resources/{i3,i3status,compton.conf} /home/$USER/.config/
+cp -r $(pwd)/resources/{i3,i3status,i3lock} /home/$USER/.config/
+cp $(pwd)/resources/compton.conf /home/$USER/.config/compton.conf
+chmod +x /home/$USER/.config/i3lock/i3lock-blur.sh

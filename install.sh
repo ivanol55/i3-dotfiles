7c12ef129d726dd606eda75037b25010c3f5f42e
Ivan <ivanm106@gmail.com>
2021-07-11 23:37:31 +0200

    hidpi scaling check and fix

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@ su root -c "apt update && apt install i3 volumeicon-alsa network-manager feh x11
 cp -r $(pwd)/resources/{i3,i3status,i3lock} /home/$USER/.config/
 cp $(pwd)/resources/compton.conf /home/$USER/.config/compton.conf
 chmod +x /home/$USER/.config/i3lock/i3lock-blur.sh
+RES=$(xdpyinfo | awk '/dimensions:/ { print $2 }')
+if [ $RES = "3840x2160" ] 
+then
+	cp resources/.Xresources /home/$USER/.Xresources	
+fi
